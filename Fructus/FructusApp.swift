731e49cd112d0b3566fbe59dd6171c88a9aad1c0
Maxim Mitin <maximus405@gmail.com>
2021-09-12 18:42:49 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Maxim Mitin on 12.09.21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
